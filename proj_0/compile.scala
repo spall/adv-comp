cc0cb3e7bde11dbf49901ab3a4e83f59bc2af3c8
Sarah Spall <sspall10@gmail.com>
2014-10-10 23:53:20 -0600

    filled out some of compile which of course matt made easy, just trying to understand all church encoding code

--- proj_0/compile.scala
+++ proj_0/compile.scala
@@ -47,27 +47,28 @@ def compile(exp : Exp) {
 	exp match {
 	    case RefExp(id) => RefExp(id)
 	    case BoolExpr(val) =>
-	    if(val) /* true */
+	    if(val) /* true */ /* true and false return lambdas */
 	    {
-		LambdaExp("t", LambdaExp("f", AppExp(
+		true()
 	    }
-	    else
+	    else /* need to define void */
 	    {
-		
+		false()
 	    }
 	    case IfExp(cond, t, f) =>
-	    	 compile(AppExp(cond, 
+	    	 compile(AppExp(cond, LambdaExp(List(), t), LambdaExp(List(), f)))
 	    case AndExp(cond1, cond2) =>
 	    	 compile(IfExp(cond1, cond2, BoolExpr(false)))
 	    case OrExp(cond1, cond2) =>
 	    	 compile(IfExp(cond1, BoolExpr(true), cond2))
-	    case IntExp(val)
-	    case ZeroPExp(test)
-	    case SubExp(exp1, exp2)
-	    case PlusExp(exp1, exp2)
-	    case TimesExp(exp1, exp2)
-	    case EqExp(exp1, exp2)
-	    case NullExp()
+	    case IntExp(val) => church_numeral(val)
+	    case ZeroPExp(test) => AppExp(zero_huh(), compile(test))
+	    case SubExp(exp1, exp2) => AppExp( AppExp(sub(), compile(exp1)), compile(exp2))
+	    case PlusExp(exp1, exp2) => AppExp( AppExp(sum(), compile(exp1)), compile(exp2))
+	    case TimesExp(exp1, exp2) => AppExp( AppExp(mul(), compile(exp1)), compile(exp2))
+	    case EqExp(exp1, exp2) => compile( AndExp( AppExp(zero_huh(), SubExp(exp1, exp2)),
+	    	 	     	      	       	       AppExp(zero_huh(), SubExp(exp2, exp1))))
+	    case NullExp() =>
 	    case ConsExp(car,cdr)
 	    case CarExp(pair)
 	    case CdrExp(pair)
@@ -105,4 +106,84 @@ def compile(exp : Exp) {
 			compile(AppExp(AppExp(fun, args.head),args.tail))
 		 }		 
 
-}	    
+}	    	
+
+def apply_n(f, n, z) /* f(f(f(z) thing */
+{
+	if( n == 0)
+	    z
+	else
+	{	
+	AppExp(f, apply_n(f, n-1, z))	
+		}
+}
+def church_numeral(n)
+{
+	if(n == 0)
+	{
+		LambdaExp(f, LambdaExpr(z, z))
+	}	
+	else
+	{
+		LambdaExp(f, LambdaExpr(z, apply_n(f n z)))	
+	}
+
+} 
+
+def true()
+{
+	LambdaExp(List("t"), LambdaExp(List("f"), AppExp( RefExp("t"), void())))
+}
+
+def false()
+{
+	LambdaExp(List("t"), LambdaExp(List("f"), AppExp(RefExp("f"), void())))
+}
+
+def zero_huh()
+{
+	LambdaExp(List("n"), AppExp( AppExp( RefExp("n"), LambdaExp(List(), false())), true()))
+}
+
+def sum()
+{
+	LambdaExp(List("n"), 
+		LambdaExp(List("m"), 
+			LambdaExp(List("f"), 
+				LambdaExp(List("z"), 
+					AppExp( AppExp( 
+						RefExp("m"), 
+						RefExp("f")), 
+					  AppExp( AppExp(RefExp("n"), 
+							     	RefExp("f")), 
+								RefExp("z")))))))
+}
+
+def mul()
+{
+	LambdaExp(List("n"),
+		LambdaExp(List("m"),
+			LambdaExp(List("f"),
+				LambdaExp(List("z"),
+					AppExp( AppExp(RefExp("m"), AppExp(RefExp("n"), RefExp("f"))),
+						RefExp("z"))))))
+}
+
+def pred()
+{
+	LambdaExp(List("n"),
+		LambdaExp(List("f"),
+			LambdaExp(List("z"),
+				AppExp( AppExp( AppExp(RefExp("n"), LambdaExp(List("g"),
+										LambdaExp(List("h"),
+											AppExp(RefExp("h"),
+												AppExp(RefExp("g"), RefExp("f"))))))
+							LambdaExp(List("u"), RefExp("z")))
+					LambdaExp(List("u"), RefExp("u"))))))
+
+}
+
+def sub()
+{
+	LambdaExp(List("n"), LambdaExp(List("m"), AppExp( AppExp(RefExp("m"), pred()), RefExp("n"))))
+}
